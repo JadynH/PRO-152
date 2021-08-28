1beb6e83d744894b00eb635e94e6360310fc7ca2
JadynH <70865644+JadynH@users.noreply.github.com>
2021-08-27 20:24:10 -0400

    Add files via upload

---
+++ Car.js
@@ -0,0 +1,19 @@
+AFRAME.registerComponent('car', {
+    schema: {
+        radius: {type:'number', default: '75'},
+        height: {type:'number', default: '3'}
+    },
+    update:function() {
+        window.addEventListener("click", e => {
+            this.data.clickCounter = this.data.clickCounter +1;
+            if (this.data.clickCounter === 1) {
+                const rotation = {x:0, y:20, z:0};
+                this.el.setAttribute("rotation", rotation);
+            }
+            else if (this.data.clickCounter === 2)
+            {
+                const rotation = {x:0, y:100, z:0};
+            }
+        })
+    }
+});
